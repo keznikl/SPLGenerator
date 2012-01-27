4902e61504e38416272e64306474136557073acf
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2012-01-27 14:28:33 +0100

    refactoring

---
+++ generators.py
@@ -0,0 +1,54 @@
+__author__ = 'Keznikl'
+
+from formula import *
+
+def several_perf_posibilites_unknown_cause(method = "", possibilities = []):
+    if not possibilities:
+        return
+    return Disjunction([
+    Conjunction([
+        Variable("P" + method + p + "1"),
+        Variable("P" + method + p + "2")
+    ])
+    for p in possibilities
+    ])
+
+def several_perf_posibilites_use_fastest(method = "", possibilities = [], cnf = False):
+    if not possibilities:
+        return
+    ret =  [
+               Conjunction(
+                   # Pa<b & Pa<c <=> Sa
+                   [Equivalence(
+                       Conjunction([
+                       Variable("P" + p + "<" + r)
+                       for r in possibilities if r != p
+                       ]),
+                       Variable("S" + p)
+                   )
+                    for p in possibilities]
+               )] + [
+        Conjunction(
+            # (Pa<b <=> !Pb<a)
+            [Equivalence(
+                Variable("P" + a + "<" + b),
+                Negation(Variable("P" + b + "<" + a))
+            )
+             for a in possibilities for b in possibilities if a != b]
+        )]  + [
+        Conjunction(
+            # (Pa<b <=> !Pb<a)
+            [Implication(
+                Variable("S" + p),
+                Conjunction([
+                    Variable("P" + method + p + "1"),
+                    Variable("P" + method + p + "2")
+                ])
+            )
+             for p in possibilities]
+        )]
+    if cnf:
+        return [f.toCNF() for f in ret]
+    else:
+        return ret
+
